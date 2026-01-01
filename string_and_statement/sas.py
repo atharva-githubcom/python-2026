8fe9e2f91b5b65585819624e58a78df40641b67c
atharva-githubcom <atharvagite9@gmail.com>
2026-01-01 10:59:41 +0530

    str and condiation statemnet

---
+++ string_and_statement/sas.py
@@ -0,0 +1,14 @@
+#WAP to input user's first name & print its length.
+#str_1=input("enter the name ")
+#print(len(str_1))
+
+#WAP to find the occurence of $ in the string.
+str_2= " $ is the name  of the American currency."
+print(str_2.count("$"))
+
+#WAP concatation of string.
+str_3="Atharva"
+str_4="Gite"
+ouput=str_3+str_4
+print(ouput)
+print(str_3[1:5])
