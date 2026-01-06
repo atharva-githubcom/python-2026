e369aafce5d22695c6eab4301ba5b619ce261fd5
atharva-githubcom <atharvagite9@gmail.com>
2026-01-06 18:42:24 +0530

    pratice

--- pratice_question/frist.py
+++ pratice_question/frist.py
@@ -58,4 +58,85 @@ print(dict)
 sets=set(map(int,input("enter the numbers:").split()))
 print(sets)
 
+# Write Python 3 code in this online editor and run it
+i=1
+even_count=0
+od_count=0
+while(i<=20):
+    if(i%2==0):
+        print(i,even_count,"even")
+        even_count +=1
+    else:
+        print(i,od_count,"the number is odd")
+        od_count +=1
+    i +=1
+print("the total count is ",even_count)    
+print("the total  count of odd is ",od_count) 
+
+
+#Print numbers from 1 to 20, but:
+#If the number is divisible by 4, print "Quad"
+#If the number is divisible by 6, print "Hex"
+#If the number is divisible by both 4 and 6, print "QuadHex"
+#Otherwise, print the number
+
+for i in range(21):
+    if(i%4==0 and i%6==0):
+        print (i,"QuadHex")
+    elif(i%4==0):
+        print(i,"quad");
+    elif(i%6==0):
+        print(i,"hex")
+    else:
+        print(i)
+        
+        
+#Task:
+#Print numbers from 1 to 10
+#Rules:
+#If number is divisible by 3 → print "Three"
+#Otherwise → print the number 
+
+for i in range(1,11):
+    if(i%3==0):
+        print(i,"three")
+    else:
+        print(i)
+        
+        
+#Task:
+#Print numbers from 1 to 15
+#Rules:
+#If number is divisible by 3 and 5 → print "ThreeFive"
+#Else if divisible by 3 → print "Three"
+#Else if divisible by 5 → print "Five"
+#Else → print the number 
+for i in range(1,16):
+    if(i%3==0 and i%5==0):
+        print(i,"Threefive")
+    elif(i%3==0):
+        print(i,"three")
+    elif(i%5==0):
+        print(i,"five")
+    else:
+        print(f"this is the number:{i}")
+        
+#Task:
+#Print numbers from 1 to 30
+#Rules:
+#If number is divisible by 2 and 3 → print "TwoThree"
+#Else if divisible by 2 → print "Two"
+#Else if divisible by 3 → print "Three"
+#Else → print the number 
+
+for i in range(1,31):
+    if (i%6==0):
+        print("twothree")
+    elif(i%2==0):
+        print("two")
+    elif(i%3==0):
+        print("three")
+    else:
+        print(i)
+
 
